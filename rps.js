563fa995e2e539ff40d0f41f37da5d76277f889a
rabbisaif <102999179+rabbisaif@users.noreply.github.com>
2023-12-03 23:27:06 -0500

    result from one game

--- rps.js
+++ rps.js
@@ -3,7 +3,7 @@ function getComputerChoice() {
     let options = ['rock', 'paper', 'scissor'];
     let randomIndex = Math.floor(Math.random() * options.length);
     let selectedOption = options[randomIndex];
-    
+
     // Return the selected option
     return selectedOption;
 }
@@ -12,11 +12,10 @@ function getComputerChoice() {
 let computerChoice = getComputerChoice();
 console.log('Computer chose: ' + computerChoice);
 
-
 function getUserChoice() {
     // Use the prompt function to get user input
     let userChoice = prompt('Enter your choice (rock, paper, or scissor):');
-    
+
     // Return the user's choice
     return userChoice;
 }
@@ -25,14 +24,40 @@ function getUserChoice() {
 let userChoice = getUserChoice();
 console.log('User chose: ' + userChoice);
 
-
-
 const TIES = 'It\'s a tie!';
+const youWin = 'You Win! Rock beats Scissor';
+const youWin1 = 'You Win! Paper beats Rock';
+const youWin2 = 'You Win! Scissor beats Paper';
+const compWin = 'Comp Win! rock beats Scissor';
+const compWin1 = 'Comp Win! Paper beats Rock';
+const compWin2 = 'Comp Win! Scissor beats Paper';
 
 function play(computerChoice, userChoice) {
     // Check if userChoice is equal to computerChoice
     if (userChoice === computerChoice) {
         // Return the constant TIES
         return TIES;
+    } else if (userChoice === "rock" && computerChoice === "scissor") {
+        return youWin;
+    } else if (userChoice === "paper" && computerChoice === "rock") {
+        return youWin1;
+    } else if (userChoice === "scissor" && computerChoice === "paper") {
+        return youWin2;
+    } else if (userChoice === "scissor" && computerChoice === "rock") {
+        return compWin;
+    } else if (userChoice === "rock" && computerChoice === "paper") {
+        return compWin1;
+    } else if (userChoice === "paper" && computerChoice === "scissor") {
+        return compWin2;
+    } else {
+        // Default case when none of the conditions match
+        return 'Invalid input or unexpected scenario.';
     }
+
 }
+
+// Call the play function with the computer and user choices
+let result = play(computerChoice, userChoice);
+
+// Log the result to the console
+console.log(result);
